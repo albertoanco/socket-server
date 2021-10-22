9d00bc3151f9dd5d19adad0481cc5705b6a6ace3
alberto_anco <albertoanco.lavisa@gmail.com>
2021-10-22 08:21:59 -0500

    Primer commit - Rest Server

---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from "./clases/server";
+//import { router } from "./routes/router";
+import router from "./routes/router";
+//import bodyParser from "body-parser";
+import cors from 'cors';
+
+var bodyParser = require('body-parser')
+
+const server = new Server();
+
+// bodyParser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+server.app.use( cors({ origin: true, credentials: true }) );
+
+// rutas de servicios
+server.app.use('/', router);
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+});
